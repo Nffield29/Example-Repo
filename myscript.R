1122b0321dd906da3783e93bac4009b243b8bc6b
Nadine <19180311@brookes.ac.uk>
2021-11-26 15:11:24 +0000

    added curve fitting and changes colours

--- myscript.R
+++ myscript.R
@@ -1,1 +1,9 @@
-plot(example_data$xdata, example_data$ydata)
+plot(example_data$xdata, example_data$ydata, col="orange")
+
+p1 = 1
+p2 = 0.2
+#do the fit
+fit = nls(ydata ~ p1*cos(p2*xdata) + p2*sin(p1*xdata), data = example_data, start = list(p1=p1, p2=p2))
+#plot the fitted line
+new = data.frame(xdata = seq(min(example_data$xdata), max(example_data$xdata), len=200))
+lines(new$xdata, predict(fit,newdata=new), col="orange")
